a23eafc12267bccb6c1dd4a9ef947a6dbf336911
Abhishekcd37 <dwivediabhishek123498765@gmail.com>
2024-07-07 14:48:23 +0000

    container config done

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,7 +2,17 @@ const accountID=112345
 let accountEmail="abhi1234@gmail.com"
 var accountpassword="223344"
 accountCity="Kanpur"
+let accountState;
 
-//accountID=2
+// accountID=2  //not allowed
 
 console.log(accountID);
+accountEmail="htc123@gmail.com"
+accountpassword="212121212"
+accountCity="Ballore"
+/*
+Prefer not to use var 
+because of the issue in block space and funtional scope
+*/
+
+console.table([accountEmail ,accountID, accountpassword, accountCity, accountState]);
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using node.js , not browser
+
+console.log(3+3)
+
+// code readability is important 
+ console.log("abhi")
+
+ let name="abhishek"//string
+ let age=22
+ let isLoggingIn=false
+
+//primitive datatypes
+ // number=> 2 to power 53
+ // bigint for bigger values
+ // string can be declared in 'single ' " and double" also
+ // boolean =>true/flase
+ // null=>standalone value
+ // undefined =>
+ // symbol=> uniquness
+
+ //object
+
+
+ console.log(typeof null);// op object
+ console.log(typeof undefined);// op undefined
