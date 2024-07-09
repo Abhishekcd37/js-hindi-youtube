351601c644f3248c1dba7012a33be15159eed4a4
Abhishekcd37 <dwivediabhishek123498765@gmail.com>
2024-07-09 07:20:26 +0000

    container config done

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2!=1)
+
+
+// console.log("2">1);
+// console.log("02">1)// during the use of comparison operator there is the
+//  possibility of unexpected output like in this example the string vlaue is
+//  comapred along with integer so performs the conversion and compares them
+
+// console.log(true==0)
+// console.log(true>0)
+// console.log(true>=0)
+
+// console.log(null ==0) //here we can see different output genrated for each condition for same input
+// console.log(null >=0) // the reason is that an equality check == and comparison > < >= <= works differently.
+// console.log(null >0) // Comparison convert the null to a number, treating it as 0.
+
+// console.log(undefined ==0)
+// console.log(undefined >=0)//it will always return false each one of the given condition
+// console.log(undefined >0)
+
+//===
+console.log("2"===2)
---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+//Primitive
+
+//7 types: String , Number , Boolean , null, Undefined, Symbol, BigInt
+
+const score=100;
+const scorevalue=100.3;
+
+const isLoggedin= false
+const outsideTemp= null
+let usermail;
+
+const id= Symbol('123');
+const anotherid= Symbol('123');
+console.log(id===anotherid)
+
+const bignumber =245433333333333333333333354643n// n is used to declare bigint
+
+console.log(typeof(bignumber))
+
+
+//Reference( Non-Primitive)
+// Array , Objects , Funtions
+
+const heros =["Shaktiman", "nagraj","Kilwish"];
+let myobj={
+    name:"Hitesh",
+    age : 22
+}
+
+const myFunction= function(){
+    console.log("hello world");
+}
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
