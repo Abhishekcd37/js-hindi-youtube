6b5b6cc9c517145a35caeb291a166415661ecdd3
Abhishekcd37 <dwivediabhishek123498765@gmail.com>
2024-07-27 05:13:37 +0000

    function talk done

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -17,10 +17,10 @@ function addTwonumbers(number, number2){
 
 const result=addTwonumbers(3,5)
 
-// console.log(result);
+// console.log(result);only when the function follow up with the return statement then only the result will be printed
 
 function loginusermessage(username){
-    if(username === undefined)
+    if(username === undefined)//(!username)shows if it is empty
     {
         console.log("please enter a valid username");
         return
@@ -31,4 +31,38 @@ function loginusermessage(username){
 
 
 // console.log(loginusermessage("Abhishek"))
-console.log(loginusermessage());
+// console.log(loginusermessage("abhi"));
+
+
+function calculateCarPrice(val1,val2,...num1){//rest operator
+    return num1
+}
+
+// console.log(calculateCarPrice(200,300,400,900,1000));//returns all the values passed in arguments as an array
+
+const user= {
+    username: "Abhishek",
+    prices: 300
+}
+
+function handleobjects(anyobject){
+    // console.log(anyobject.username);
+    console.log(`Name of the user ${anyobject.username} and the price is ${anyobject.price}`)
+    return
+}
+
+// handleobjects(user)
+handleobjects({
+    username:"Rahul Agnihotri",
+    price: 400
+})
+
+
+const myNewArray= [2000,300,400,500]
+
+function returnarray(getarray){
+    return getarray[2]
+}
+
+// console.log(returnarray(myNewArray));
+// console.log(returnarray([200,400,500,6000]));
